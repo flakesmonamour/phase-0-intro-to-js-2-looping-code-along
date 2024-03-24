b6e4bfcc0d43cf82d6beca64cf860f827e4aeeb9
flakesmonamour <mwangimark456@gmail.com>
2024-03-24 19:01:57 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie","Aki"];
+function writeCards(names) {
+    return names.map(name => {
+      return `Thank you, ${name}, for the wonderful surprise gift!`;
+    });
+    
+  }
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
